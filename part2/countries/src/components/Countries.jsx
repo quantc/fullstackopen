8a0a84c39e5019cab273c708548ed039b9ba36e9
Andrzej Kociuba <andrzej.kociuba@gft.com>
2024-06-11 23:38:06 +0200

    2.19 completed

--- part2/countries/src/components/Countries.jsx
+++ part2/countries/src/components/Countries.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react"
+
 const Countries = ({ countriesList, filter }) => {
+  const [countryToShow, setCountryToShow] = useState("")
   let countriesFiltered = []
 
   if (filter.length > 0) {
@@ -10,37 +13,62 @@ const Countries = ({ countriesList, filter }) => {
   if (countriesFiltered.length > 10) {
     return "Too many matches, specify another filter"
   } else if (countriesFiltered.length === 1) {
-    const found = countriesFiltered[0]
-    const languages = Object.values(found.languages)
-
-    return (
-      <div>
-        <div>
-          <h1>{found.name.common}</h1>
-          capital {found.capital} <br />
-          area {found.area}
-        </div>
-        <br />
-        <div>
-          <b>languages:</b>
-          <ul>
-            {languages.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
-        </div>
-        <img
-          src={found.flags.png}
-          alt={found.flags.alt}
-          style={{ width: "30%", height: "auto", border: "1px solid black" }}
-        />
-      </div>
-    )
+    return <CountryInfo country={countriesFiltered[0]} />
   }
 
-  return countriesFiltered.map((c) => {
-    return <div key={c.name.common}>{c.name.common}</div>
-  })
+  const selectedCountry = countriesFiltered.find(
+    (c) => c.name.common === countryToShow
+  )
+
+  return (
+    <>
+      {countriesFiltered.map((c) => {
+        return (
+          <div key={c.name.common}>
+            {c.name.common + " "}
+            <button
+              value="show"
+              onClick={() => setCountryToShow(c.name.common)}
+            >
+              show
+            </button>
+          </div>
+        )
+      })}
+
+      <CountryInfo country={selectedCountry} />
+    </>
+  )
+}
+
+const CountryInfo = ({ country }) => {
+  if (!country) return
+
+  const languages = Object.values(country.languages)
+
+  return (
+    <div>
+      <div>
+        <h1>{country.name.common}</h1>
+        capital {country.capital} <br />
+        area {country.area}
+      </div>
+      <br />
+      <div>
+        <b>languages:</b>
+        <ul>
+          {languages.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+      <img
+        src={country.flags.png}
+        alt={country.flags.alt}
+        style={{ width: "30%", height: "auto", border: "1px solid black" }}
+      />
+    </div>
+  )
 }
 
 export default Countries
